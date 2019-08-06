c09605ed09a7eb215452a08d5bdf3963dcf8acae
fannitormasi <f.tormasi@euromacc.hu>
2019-08-06 14:39:52 +0200

    adding new files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {EventComponent} from './event/event.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    EventComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [EventComponent]
 })
 export class AppModule { }
---
+++ src/app/event/event.component.ts
@@ -0,0 +1,20 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-event',
+  templateUrl: './event.component.html',
+  styleUrls: ['./event.component.css']
+})
+
+export class EventComponent {
+  title = 'sziasztok, mi itt vagyunk';
+
+  constructor() {
+    console.log(`${this.title} itt lehet folytatni folyó szöveggel azt amit ki szeretnénk írni`);
+    this.greetings();
+  }
+
+  greetings() {
+    console.log('not implemented yet!');
+  }
+}
